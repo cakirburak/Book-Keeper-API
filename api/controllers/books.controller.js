2794d616a3b3dcc1df61c7b2962db632dc7e249c
cakirburak <cakirburak43@hotmail.com>
2024-04-04 09:48:05 +0300

    avarage book score to get books method

--- api/controllers/books.controller.js
+++ api/controllers/books.controller.js
@@ -1,4 +1,6 @@
+import { QueryTypes } from "sequelize";
 import { Book } from "../../db/models/book.model.js";
+import { sequelize } from "../../db/sequelize.config.js";
 import { validateBookName } from "../utils/validateReqBody.js"
 
 export const getBooks = async (req, res) => {
@@ -14,15 +16,34 @@ export const getBooks = async (req, res) => {
 
 export const getBook = async (req, res) => {
   const { book_id: bookId } = req.params
+  let book = null;
 
   try {
-    const book = await Book.findByPk(bookId);
-    if (book)
-      res.status(200).json(book);
-    else
+    book = await Book.findByPk(bookId);
+    if (!book)
       res.status(404).json({ "Not Found": `book id not found : ${bookId}` });
   } catch (error) {
     res.status(500).json({ "Internal Server Error": error.message });
+  }
+
+  try {
+    const avarageScore = await sequelize.query('SELECT book_id, avg(book_score) AS score FROM borrow_stats WHERE book_id = :bookId AND borrow_stats.is_returned = true GROUP BY book_id',
+      {
+        replacements: { bookId: bookId },
+        type: QueryTypes.SELECT
+      }
+    );
+    if (avarageScore.length > 0) {
+      const bookDataValues = book.get({ plain: true });
+      const score = parseFloat(avarageScore[0].score).toString();
+      const { isAvailable, ...bookWithoutIsAvailable } = bookDataValues;
+      console.log(bookWithoutIsAvailable);
+      res.status(200).json({ ...bookWithoutIsAvailable, score });
+    } else {
+      res.status(200).json({ ...bookWithoutIsAvailable, "score" : -1 });
+    }
+  } catch (error) {
+    
   }
 }
 
